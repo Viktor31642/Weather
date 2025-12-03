9de929c4882ec9149dda802eec052eb00d86a008
Viktor31642 <viktor31642@gmail.com>
2025-12-03 13:34:03 +0200

    add test

---
+++ WeatherApp.Tests/WeatherParsingTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using Xunit;
+public class WeatherParsingTests
+{
+    [Fact]
+    public void Deserialize_ValidJson_FillsAllFields()
+    {
+        string json = @"{
+          ""name"": ""Lviv"",
+          ""main"": { ""temp"": 15.5 },
+          ""weather"": [
+            { ""description"": ""clear sky"" }
+          ]
+        }";
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        WeatherResponse? result = JsonSerializer.Deserialize<WeatherResponse>(json, options);
+
+        Assert.NotNull(result);
+        Assert.NotNull(result!.main);
+        Assert.NotNull(result.weather);
+        Assert.NotEmpty(result.weather);
+
+        Assert.Equal(15.5, result.main.temp);
+        Assert.Equal("clear sky", result.weather[0].description);
+        Assert.Equal("Lviv", result.name);
+    }
+}
